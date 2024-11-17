06681260f0a0cb6d2128a5b2f9eded8bf083499a
Nichole <nchnicolini@gmail.com>
2024-11-16 23:03:56 -0300

    dois gráficos feitos

--- py/analises.py
+++ py/analises.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 def ler_arquivo_csv(nome_arquivo):
     """
     Função para ler o arquivo CSV e retornar um DataFrame com os dados.
@@ -17,6 +18,7 @@ def ler_arquivo_csv(nome_arquivo):
     df = pd.read_csv(caminho)
     return df
 
+
 def criar_grafico_ocorrencias_por_ano():
     """
     Função para criar um gráfico de barras sobre a distribuição de ocorrências por ano e retornar o HTML.
@@ -27,21 +29,23 @@ def criar_grafico_ocorrencias_por_ano():
         return "<p>Erro ao carregar os dados.</p>"
 
     # Agrupa os dados por ano e conta a quantidade de ocorrências
-    dados_agrupados = dados.groupby("ANO")["CONTAGEM"].sum().reset_index(name='total_ocorrencias')
+    dados_agrupados = dados.groupby(
+        "ANO")["CONTAGEM"].sum().reset_index(name='total_ocorrencias')
 
     # Cria o gráfico de barras
     fig = px.bar(
         dados_agrupados,
         x="ANO",
         y="total_ocorrencias",
-        title="Distribuição de Ocorrências de Suicídio Por Ano",
         color_discrete_sequence=['#4B0082']
     )
 
     # Estilo e configuração do gráfico
     fig.update_layout(
-        plot_bgcolor='rgba(18,18,30,1)',        # Cor de fundo do gráfico (azul escuro)
-        paper_bgcolor='rgba(10,10,25,1)',       # Cor de fundo do "papel" (preto-azulado)
+        # Cor de fundo do gráfico (azul escuro)
+        plot_bgcolor='rgba(18,18,30,1)',
+        # Cor de fundo do "papel" (preto-azulado)
+        paper_bgcolor='rgba(10,10,25,1)',
         font=dict(
             family="Arial",                     # Fonte do texto
             size=12,                            # Tamanho da fonte
@@ -60,8 +64,10 @@ def criar_grafico_ocorrencias_por_ano():
             title="Total de Ocorrências",       # Título do eixo Y
             color="#E1E1FF",                    # Cor dos ticks e do título do eixo Y
             showgrid=True,                      # Mantém a grade horizontal para orientação
-            gridcolor="rgba(68,68,90,0.5)"      # Cor da grade (cinza azulado com transparência)
-        )
+            # Cor da grade (cinza azulado com transparência)
+            gridcolor="rgba(68,68,90,0.5)"
+        ),
+
     )
 
     # Converte o gráfico em HTML
@@ -70,28 +76,30 @@ def criar_grafico_ocorrencias_por_ano():
 
 
 def gerar_grafico_local():
-   
+
     dados = ler_arquivo_csv("cleaned_result.csv")
     if dados is None:
         return "<p>Erro ao carregar os dados.</p>"
-    
+
     # Agrupar os dados por local e somar as ocorrências
-    dados_agrupados = dados.groupby("LOCAL")["CONTAGEM"].sum().reset_index(name='total_ocorrencias')
-    
+    dados_agrupados = dados.groupby(
+        "LOCAL")["CONTAGEM"].sum().reset_index(name='total_ocorrencias')
+
     # Criar o gráfico de pizza
     fig = px.pie(
-        dados_agrupados, 
-        names='LOCAL', 
-        values='total_ocorrencias', 
-        title="Distribuição De Suicídios Por Local",
+        dados_agrupados,
+        names='LOCAL',
+        values='total_ocorrencias',
         color='LOCAL',  # Cores distintas para os locais
         color_discrete_sequence=px.colors.qualitative.Set3  # Cores definidas
     )
 
     # Estilo e configuração do gráfico
     fig.update_layout(
-        plot_bgcolor='rgba(18,18,30,1)',        # Cor de fundo do gráfico (azul escuro)
-        paper_bgcolor='rgba(10,10,25,1)',       # Cor de fundo do "papel" (preto-azulado)
+        # Cor de fundo do gráfico (azul escuro)
+        plot_bgcolor='rgba(18,18,30,1)',
+        # Cor de fundo do "papel" (preto-azulado)
+        paper_bgcolor='rgba(10,10,25,1)',
         font=dict(
             family="Arial",                     # Fonte do texto
             size=12,                            # Tamanho da fonte
@@ -100,4 +108,6 @@ def gerar_grafico_local():
         title_font=dict(size=20, color="#A29BFE")  # Fonte e cor do título
     )
 
-    return fig
+    # Converte o gráfico em HTML
+    gerar_gr_html = pio.to_html(fig, full_html=False)
+    return gerar_gr_html
